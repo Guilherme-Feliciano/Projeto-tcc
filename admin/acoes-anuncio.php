cee2c8dab1e6a9c953a293f6b4a6b0ec0fe5ff67
Guilherme <guilherme.godoy.trade@gmail.com>
2021-10-27 22:22:14 -0300

    ajustes

--- admin/acoes-anuncio.php
+++ admin/acoes-anuncio.php
@@ -9,29 +9,11 @@ $descricao = $_POST['descricao'];
 $cod_login = $_SESSION['cod_login'];
 $operacao = $_POST['operacao'] ?: $_GET['operacao'];
 
-$imagem = $_FILES['imagem']['name']; //nome original
-$imagem_temporaria = $_FILES['imagem']['tmp_name']; // nome temporário 
-
-// obter a extensão do arquivo
-$extensao_imagem = strtolower(strrchr($imagem,"."));
-$novo_nome = "imagem_".time().$extensao_imagem;
-
-
-
 if ($operacao == 'cadastrar') {
 
-
-// fazer o upload da imagem 
-if(strlen($imagem) > 0 ){
-    copy($imagem_temporaria,"../imagens/$novo_nome");
-}else{
-    $novo_nome = "";
-}
-
-
     $sql = "INSERT INTO tbl_produto
-                        (categoria_produto,nome_poduto,preco,descricao,produto_usuario,imagem)
-                        VALUES ('$categoria_produto','$nome_produto','$preco','$descricao','$cod_login','$novo_nome')";
+                        (categoria_produto,nome_poduto,preco,descricao,produto_usuario)
+                        VALUES ('$categoria_produto','$nome_produto','$preco','$descricao','$cod_login')";
 
 
 
